a7a2da9804c0255a74257127ff2a200a03cade6b
zzwhoops-home <zd@5rock.org>
2024-12-09 22:34:11 -0500

    Spin implemented

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -16,7 +16,9 @@ public class Spin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        curSpeed = 0f;
+        timeElapsed = 0f;
+        direction = 1;
     }
 
     // Update is called once per frame
@@ -25,7 +27,25 @@ public class Spin : MonoBehaviour
         // add time
         timeElapsed += Time.deltaTime;
 
-        float angle = curSpeed * timeElapsed * Time.deltaTime;
+        if (timeElapsed < rampDuration)
+        {
+            curSpeed = Mathf.Lerp(0f, maxSpeed, timeElapsed / rampDuration);
+        }
+        else if (timeElapsed <= rampDuration + reverseDelay)
+        {
+            curSpeed = maxSpeed;
+        }
+        else if (timeElapsed <= 2 * rampDuration + reverseDelay)
+        {
+            curSpeed = Mathf.Lerp(maxSpeed, 0f, (timeElapsed - rampDuration - reverseDelay));
+        }
+        else
+        {
+            direction *= -1;
+            timeElapsed = 0f;
+        }
+
+        float angle = curSpeed * direction * Time.deltaTime;
         transform.rotation *= Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
